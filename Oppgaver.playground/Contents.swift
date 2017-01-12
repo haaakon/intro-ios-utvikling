ef707857644313eba529fbee78090daf78e7a05e
Håkon Bogen <bogen@beiningbogen.no>
2017-01-12 13:58:54 +0100

    Add tasks for iOS

--- Oppgaver.playground/Contents.swift
+++ Oppgaver.playground/Contents.swift
@@ -30,7 +30,25 @@ import UIKit
 // Oppgave 7:
 // Skriv en funksjon som tar en Int som argument, og skriver ut forskjellige tekster basert pa om den er større, mindre eller lik 100
 
-// Oppgave 8: 
 
+// #iOS
 
+// Oppgave 1
+// Lag en nytt prosjekt og koble en label med IBOutlet slik at du kan endre den
 
+// Oppgave 2
+// Lag en knapp som kan trykkes på, koble action slik at en funksjon i koden din kalles når bruker trykker på knappen
+// Hint : didTouchUpInside
+
+// Oppgave 3
+// Endre teksten på en label når bruker trykker en knapp
+
+// Oppgave 4
+// legg til 3 knapper, og endre label til å bli teksten til den knappen som bruker trykker på
+
+// Oppgave 5
+// Lagre et [String] array, gjør det slik at hver gang bruker trykker på en knapp,
+// så viser en label en random tekst fra arrayet
+
+// Oppgave 6
+// Legg til et UITextField, det brukeren skriver inn skal gå til en UILabel, når brukeren trykker på en knapp
